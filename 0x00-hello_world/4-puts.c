eb4d0382d24073018ae4bdb693c706a02100733f
Amogelang <mandlaziamogelang50@gmail.com>
2022-09-09 12:37:14 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - print the string in he put function
+*
+* Description: using the main function
+* this peogram prints "Programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+     puts("\"Programming is like building a multilingual puzzle");
+     return (0);
+}
